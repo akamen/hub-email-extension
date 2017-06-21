0f086a906ad454c4c4932a970d36dbc7ac81dfba
Patrick Conway <pconway@pcm-pconway-1.local>
2017-06-21 13:26:51 -0400

    wget

--- github_auto_release.sh
+++ github_auto_release.sh
@@ -138,11 +138,14 @@ if [[ "$RELEASE_VERSION" =~ [0-9]+[.][0-9]+[.][0-9]+ ]] && [[ "$RELEASE_VERSION"
 			if [ -z $WGET ]; then
 				if [ "$OS_TYPE" == "darwin" ]; then
 					curl -O http://ftp.gnu.org/gnu/wget/wget-1.19.tar.gz
-					tar xvzf wget-1.19.tar.gz
+					tar -xzf wget-1.19.tar.gz
 					cd wget-1.19
-					./configure --with-ssl=openssl --with-libssl-prefix=~/local/ssl
+					./configure --with-ssl=openssl
+					./configure --with-ssl=openssl --with-libssl-prefix=/usr/local/ssl
 					make
 					sudo make install
+					wget --help
+					cd .. && rm -rf wget*
 				elif [ "$OS_TYPE" == "linux" ]; then
 					echo "intsall wget for linux"
 				fi
